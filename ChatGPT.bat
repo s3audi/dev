3a3e6df9309a082612550a23788099fa434b35e6
s3audi <s3audi@hotmail.com>
2025-05-10 09:36:37 +0300

    Nereye gider

---
+++ ChatGPT.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Users\ZEO\AppData\Local\Maxthon\Maxthon.exe" "https://www.google.com"
+start "" "C:\Users\ZEO\AppData\Local\Maxthon\Maxthon.exe" "https://www.youtube.com"
+exit
