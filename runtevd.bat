536557b56986902a8e00b6799ae510dbe5d2ec0d
s3audi <s3audi@hotmail.com>
2025-05-12 20:26:24 +0300

    runtevd.bat eklendi

---
+++ runtevd.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Users\ZEO\AppData\Local\Maxthon\Maxthon.exe" "https://chatgpt.com/c/68199f29-bf58-8001-b438-95b9a6cf33e7"
+start "" "C:\Users\ZEO\AppData\Local\Maxthon\Maxthon.exe" "https://www.youtube.com"
+exit
